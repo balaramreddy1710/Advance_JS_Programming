a291d429cbb82b0c3b278efbb1bc94f3a88349d4
balaram <balaramreddy1710@gmail.com>
2025-04-21 16:26:37 +0530

    [Balaram] Add: Searching a value in LinkedList

---
+++ Day-01/LinkedList/search.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    while (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  print() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.data + " -> ";
+      current = current.next;
+    }
+    console.log(result + "null");
+  }
+
+  search(data) {
+    let current = this.head;
+    let count = 0;
+    while (current) {
+      if (current.data == data) return count;
+      current = current.next;
+      count++;
+    }
+    return false;
+  }
+}
+
+const L1 = new LinkedList();
+L1.append(10);
+L1.append(15);
+L1.append(20);
+L1.append(30);
+L1.print();
+console.log(L1.search(20));
