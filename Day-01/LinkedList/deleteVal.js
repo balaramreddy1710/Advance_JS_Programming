a336a18af4348fba323a8619575908e4658cc097
balaram <balaramreddy1710@gmail.com>
2025-04-21 15:29:34 +0530

    [Balaram] Add: Deleting a value of a linkedlist

---
+++ Day-01/LinkedList/deleteVal.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    while (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  delete(data) {
+    if (!this.head) return;
+
+    if (this.head.data === data) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.next && current.next.data != data) {
+      current = current.next;
+    }
+
+    if (current.next) {
+      current.next = current.next.next;
+    }
+  }
+
+  print() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.data + " -> ";
+      current = current.next;
+    }
+    console.log(result + "null");
+  }
+}
+
+const L1 = new LinkedList();
+L1.append(10);
+L1.append(15);
+L1.append(20);
+L1.print();
+L1.delete(15);
+L1.print();
