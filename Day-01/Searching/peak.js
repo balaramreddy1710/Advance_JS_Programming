5ee54f57868f58ef2114cab2e684329ef3f1d289
balaram <balaramreddy1710@gmail.com>
2025-04-21 12:01:43 +0530

    [Balaram] Add: Program to find the peak element

---
+++ Day-01/Searching/peak.js
@@ -0,0 +1,18 @@
+function findPeakElement(arr) {
+  let left = 0,
+    right = arr.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] < arr[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+}
+
+console.log(findPeakElement([1, 2, 3, 1]));
+console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4]));
