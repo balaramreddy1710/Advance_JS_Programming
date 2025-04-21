a89fd3ea535bda6b3dd38c252c77794f4ce08be8
balaram <balaramreddy1710@gmail.com>
2025-04-21 14:58:06 +0530

    [Balaram] Add: Program to implement merge sort using recursion

---
+++ Day-01/Array/recursiveMerge.js
@@ -0,0 +1,29 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [],
+    leftIndex = 0,
+    rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] <= right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+console.log(mergeSort([64, 34, 25, 12, 22, 11, 90]));
