628b3427ef9eb83d1fd93f4056198d450e3e4329
balaram <balaramreddy1710@gmail.com>
2025-04-22 14:57:08 +0530

    [Balaram] Add: Insertion at start in circular linkedlist

---
+++ Day-02/CircularLinkedList/insertBegin.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class CLL {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      newNode.next = this.head;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next != this.head) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+    newNode.next = this.head;
+  }
+
+  prepend(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      newNode.next = this.head;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next != this.head) {
+      current = current.next;
+    }
+    newNode.next = this.head;
+    current.next = newNode;
+    this.head = newNode;
+  }
+
+  print() {
+    if (!this.head) console.log("The list is empty");
+    let current = this.head;
+    let res = "";
+    do {
+      res += current.data + " -> ";
+      current = current.next;
+    } while (current != this.head);
+    console.log(res + "(head)");
+  }
+}
+
+const C = new CLL();
+C.append(10);
+C.append(20);
+C.print();
+C.prepend(5);
+C.print();
