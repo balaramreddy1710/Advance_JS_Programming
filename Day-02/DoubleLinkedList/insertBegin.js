a83806b036c912f3b813a69c037deea7d6a7b05f
balaram <balaramreddy1710@gmail.com>
2025-04-22 11:52:29 +0530

    [Balaram] Add: Program to insert at the beginning of a double linkedlist

---
+++ Day-02/DoubleLinkedList/insertBegin.js
@@ -0,0 +1,57 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      return;
+    }
+
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+  }
+
+  prepend(data) {
+    let newNode = new Node(data);
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      return;
+    }
+
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+  }
+
+  print() {
+    let current = this.head;
+    let res = "";
+    while (current) {
+      res += current.data + " <-> ";
+      current = current.next;
+    }
+    console.log(res + "null");
+  }
+}
+
+const D = new DLL();
+D.append(10);
+D.append(20);
+D.append(30);
+D.print();
+D.prepend(40);
+D.print();
