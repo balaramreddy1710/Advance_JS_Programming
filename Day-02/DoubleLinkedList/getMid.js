9cc773aa6f8d8ae49f1f502cdedc113c928052c1
balaram <balaramreddy1710@gmail.com>
2025-04-22 14:16:15 +0530

    [Balaram] Add: Program to get the middle elements of double linkedlist

---
+++ Day-02/DoubleLinkedList/getMid.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = this.tail = newNode;
+      return;
+    }
+
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+  }
+
+  getMid() {
+    let slow = this.head;
+    let fast = this.head;
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow ? slow.data : null;
+  }
+
+  print() {
+    let current = this.head;
+    let res = "";
+    while (current) {
+      res += current.data + " <-> ";
+      current = current.next;
+    }
+    console.log(res + "null");
+  }
+}
+
+const D = new DLL();
+D.append(10);
+D.append(20);
+D.append(30);
+D.append(40);
+D.print();
+console.log(D.getMid());
