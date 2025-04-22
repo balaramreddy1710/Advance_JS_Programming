5859cfd5024f52326862475d432b986f06d2cf2f
balaram <balaramreddy1710@gmail.com>
2025-04-22 14:45:26 +0530

    [Balaram] Add: Insertion at end in circular linkedlist

---
+++ Day-02/CircularLinkedList/insertEnd.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class CLL {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      newNode.next = this.head;
+      return;
+    }
+
+    let current = this.head;
+    while (current != this.head) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+    newNode.next = this.head;
+  }
+
+  print() {
+    let current = this.head;
+    let res = "";
+    do {
+      res += current.data + " -> ";
+      current = current.next;
+    } while (current != this.head);
+    console.log(res + "(head)");
+  }
+}
+
+const C = new CLL();
+C.append(10);
+C.append(20);
+C.print();
