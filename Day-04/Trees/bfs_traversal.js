6abe0ade0ba1ce0f2d740e20c44bf43effb9f683
balaram <balaramreddy1710@gmail.com>
2025-04-25 15:24:00 +0530

    [Balaram] Modified: BFS traversal of tree in js

---
+++ Day-04/Trees/bfs_traversal.js
@@ -0,0 +1,26 @@
+class Tree {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function levelOrder(root) {
+  if (!root) return null;
+  let queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    console.log(node.val);
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+}
+
+let root = new Tree(1);
+root.left = new Tree(2);
+root.right = new Tree(3);
+root.left.left = new Tree(4);
+root.left.right = new Tree(5);
+
+levelOrder(root);
