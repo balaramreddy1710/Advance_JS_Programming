d8e7daae3428cc9cd2c9d3f7413743e5080ccb95
balaram <balaramreddy1710@gmail.com>
2025-04-26 14:23:54 +0530

    [Balaram] Add: Removal of a edge between 2 vertices in graph

---
+++ Day-05/Graphs/removeEdge.js
@@ -0,0 +1,42 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v != v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v != v1);
+  }
+}
+
+const g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+console.log("Adjacency List:", g.adjacencyList);
+
+g.removeEdge("D", "E");
+console.log("Adjacency List after removing edge D-E:", g.adjacencyList);
