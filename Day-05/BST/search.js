7bb2e80019b1084fde13b79d6c688502ed57d01a
balaram <balaramreddy1710@gmail.com>
2025-04-26 10:55:49 +0530

    [Balaram] Add: Searching an element in BST

---
+++ Day-05/BST/search.js
@@ -0,0 +1,28 @@
+class treeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function insertBST(root, val) {
+  if (!root) return new treeNode(val);
+  if (val < root.val) root.left = insertBST(root.left, val);
+  else root.right = insertBST(root.right, val);
+  return root;
+}
+
+function search(root, val) {
+  if (!root || root.val === val) return root;
+  if (val < root.val) return search(root.left, val);
+  else return search(root.right, val);
+}
+
+let root = null;
+const values = [20, 10, 30, 5, 15, 25, 35];
+for (let val of values) {
+  root = insertBST(root, val);
+}
+
+console.log(search(root, 25));
