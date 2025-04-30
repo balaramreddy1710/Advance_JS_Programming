849562eb56b77af65dc3cf9f430ea864014fc628
balaram <balaramreddy1710@gmail.com>
2025-04-30 11:22:02 +0530

    [Balaram] Add: Program to understand enumerable flag

---
+++ Day-08/enumerable.js
@@ -0,0 +1,12 @@
+let user = {
+  name: "John",
+  toString() {
+    return this.name;
+  },
+};
+
+Object.defineProperty(user, "toString", {
+  enumerable: false,
+});
+
+for (let key in user) console.log(key);
