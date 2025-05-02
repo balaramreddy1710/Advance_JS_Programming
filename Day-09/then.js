35130ef3e19b0d7422bfdf6951456016411db357
balaram <balaramreddy1710@gmail.com>
2025-05-02 12:49:13 +0530

    [Balaram] Modified: Added reject code also in the program

--- Day-09/then.js
+++ Day-09/then.js
@@ -2,7 +2,16 @@ let promise = new Promise(function (resolve, reject) {
   setTimeout(() => resolve("done!"), 1000);
 });
 
+let promise1 = new Promise(function (resolve, reject) {
+  setTimeout(() => reject(new Error("Whoops!")), 1000);
+});
+
 promise.then(
+  (result) => console.log(result),
+  (error) => console.log(error)
+);
+
+promise1.then(
   (result) => console.log(result),
   (error) => console.log(error)
 );
