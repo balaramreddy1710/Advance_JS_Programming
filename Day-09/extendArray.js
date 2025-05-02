f4e442ef08f76be832cf9135153a6834d72786e2
balaram <balaramreddy1710@gmail.com>
2025-05-02 10:54:03 +0530

    [Balaram] Add: Extend the Array prototype and creating another array

---
+++ Day-09/extendArray.js
@@ -0,0 +1,12 @@
+class PowerArray extends Array {
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+console.log(arr.isEmpty());
+
+let filteredArr = arr.filter((item) => item >= 10);
+console.log(filteredArr);
+console.log(filteredArr.isEmpty());
