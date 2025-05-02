9285a951fa8ebf4dff6a88f0a29a362b92c5ed9b
balaram <balaramreddy1710@gmail.com>
2025-05-02 10:58:06 +0530

    [Balaram] Add: Using static getters to extend the Array prototype and creating another Array

---
+++ Day-09/staticGetter.js
@@ -0,0 +1,18 @@
+class PowerArray extends Array {
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  static get [Symbol.species]() {
+    return Array;
+  }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+console.log(arr.isEmpty());
+
+let filteredArr = arr.filter((item) => item >= 10);
+
+console.log(filteredArr);
+
+console.log(filteredArr.isEmpty());
