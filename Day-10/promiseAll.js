0d85c801c669e348ee20e665aaeae269816228ec
balaram <balaramreddy1710@gmail.com>
2025-05-03 14:39:20 +0530

    [Balaram] Add: Program to access multiple promises

---
+++ Day-10/promiseAll.js
@@ -0,0 +1,16 @@
+let names = ["iliakan", "remy", "jeresig"];
+
+let requests = names.map((name) =>
+  fetch(`https://api.github.com/users/${name}`)
+);
+
+Promise.all(requests)
+  .then((responses) => {
+    for (let response of responses) {
+      console.log(`${response.url}: ${response.status}`);
+    }
+
+    return responses;
+  })
+  .then((responses) => Promise.all(responses.map((r) => r.json())))
+  .then((users) => users.forEach((user) => console.log(user.name)));
